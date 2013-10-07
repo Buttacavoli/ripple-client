dbac3c414cca69b07dfb7e17a51974624e08f6b9
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-10-07 09:59:41 -0700

    M2020

---
+++ src/js/tabs/m2020.js
@@ -0,0 +1,90 @@
+var util = require('util');
+var Tab = require('../client/tab').Tab;
+
+var M2020Tab = function ()
+{
+  Tab.call(this);
+};
+
+util.inherits(M2020Tab, Tab);
+
+M2020Tab.prototype.pageMode = 'single';
+M2020Tab.prototype.parent = 'main';
+
+M2020Tab.prototype.generateHtml = function ()
+{
+  return require('../../jade/tabs/m2020.jade')();
+};
+
+M2020Tab.prototype.angular = function (module) {
+  module.controller('M2020Ctrl', ['$scope', '$element', '$routeParams', '$location', 'rpId', 'rpNetwork', '$rootScope', 'rpZipzap',
+  function ($scope, $element, $routeParams, $location, $id, $network, $rootScope, $zipzap)
+  {
+    var SnapSwapAddress = 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q';
+
+    $scope.mode = 'form';
+    $scope.passwordSet = {};
+
+    $scope.register = function() {
+
+      // Create ripple wallet
+      $id.register($scope.username, $scope.password1, function(key){
+        console.log('ripple account has been created',key);
+        // Login to ripple wallet
+        $id.login($scope.username, $scope.password1, function (err, blob) {
+          console.log('logged in');
+          // Create zipzap account, fund ripple wallet
+          $zipzap.register($id.account);
+          $zipzap.request(function(){
+            console.log('zipzap account has been created');
+            // TODO fund the account
+            console.log('trusting...');
+            // Trust SnapSwap
+            var amount = '100/USD/' + SnapSwapAddress;
+            var tx = $network.remote.transaction();
+            tx.ripple_line_set($id.account, amount)
+              .on('proposed', function(res){
+                console.log('trusting proposed', res);
+                $scope.$apply(function () {
+                  // Remember currency and increase order
+                  var found;
+
+                  for (var i = 0; i < $scope.currencies_all.length; i++) {
+                    if ($scope.currencies_all[i].value.toLowerCase() == 'usd') {
+                      $scope.currencies_all[i].order++;
+                      found = true;
+                      break;
+                    }
+                  }
+
+                  if (!found) {
+                    $scope.currencies_all.push({
+                      "name": currency,
+                      "value": currency,
+                      "order": 1
+                    });
+                  }
+                });
+
+                // Add SnapSwap as a contact
+                $scope.userBlob.data.contacts.unshift({
+                  name: 'SnapSwap',
+                  address: SnapSwapAddress
+                });
+
+                $scope.mode = 'registerSuccess';
+              })
+              .on('error',function(err){
+                console.log('error',err);
+              })
+              .submit();
+          })
+        })
+      });
+
+      $scope.mode = 'registerProgress';
+    }
+  }]);
+};
+
+module.exports = M2020Tab;
